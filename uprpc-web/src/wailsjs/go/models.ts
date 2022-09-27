93df7d16663b57df869d0e9f9f76a61b0cf0fb61
deific <deific@126.com>
2022-09-27 18:36:10 +0800

    add Send

--- uprpc-web/src/wailsjs/go/models.ts
+++ uprpc-web/src/wailsjs/go/models.ts
@@ -19,3 +19,38 @@ export namespace api {
 
 }
 
+export namespace client {
+	
+	export class RequestData {
+	    id: string;
+	    protoPath: string;
+	    namespace: string;
+	    serviceName: string;
+	    methodName: string;
+	    methodMode: number;
+	    host: string;
+	    body: string;
+	    mds: string;
+	    IncludeDirs: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new RequestData(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.protoPath = source["protoPath"];
+	        this.namespace = source["namespace"];
+	        this.serviceName = source["serviceName"];
+	        this.methodName = source["methodName"];
+	        this.methodMode = source["methodMode"];
+	        this.host = source["host"];
+	        this.body = source["body"];
+	        this.mds = source["mds"];
+	        this.IncludeDirs = source["IncludeDirs"];
+	    }
+	}
+
+}
+

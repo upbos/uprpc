f5c6e59ab02fbce4b39ee8a5409c41edfb1b4211
deific <deific@126.com>
2022-10-02 11:12:34 +0800

    fix close

--- uprpc-web/src/wailsjs/go/models.ts
+++ uprpc-web/src/wailsjs/go/models.ts
@@ -1,9 +1,30 @@
+export namespace main {
+	
+	export class R {
+	    success?: boolean;
+	    message?: string;
+	    data?: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new R(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.success = source["success"];
+	        this.message = source["message"];
+	        this.data = source["data"];
+	    }
+	}
+
+}
+
 export namespace client {
 	
 	export class Metadata {
 	    id?: number;
 	    key?: string;
-	    value: string;
+	    value: number[];
 	    parseType?: number;
 	
 	    static createFrom(source: any = {}) {
@@ -65,27 +86,6 @@ export namespace client {
 		    }
 		    return a;
 		}
-	}
-
-}
-
-export namespace main {
-	
-	export class R {
-	    success?: boolean;
-	    message?: string;
-	    data?: any;
-	
-	    static createFrom(source: any = {}) {
-	        return new R(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.success = source["success"];
-	        this.message = source["message"];
-	        this.data = source["data"];
-	    }
 	}
 
 }
